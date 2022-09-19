1905e55e092d186363e0a2dd2c038d534b6466c4
guilherme-fcm <guilherme_fcm@hotmail.com>
2022-09-19 20:30:31 -0300

    Add nodemon

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
+const http = require('http')
 const socket = require('socket.io')
 
 const app = express()
 
-app.listen(3000, () => { console.log('Server started!') })
+app.listen(3000, () => console.log('Server started!'))
